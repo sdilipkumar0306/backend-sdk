cb506fcaca8866dae9bc0a4de3f3ea5b77581196
sdk0306 <dilipkumar0306198@gmail.com>
2022-01-16 20:55:11 +0530

    sdk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8001;
 
-app.get("/", (req, res) => res.status(200).send("app running sucessfully"));
+app.get("/", (req, res) => res.status(200).send("app running sucessfully on port 8001"));
 app.listen(port, () => console.log(`app running at http://localhost:${port}`));
